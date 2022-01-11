8da153815388aee1b822a92b0b2eae41f1c19415
Kyle <khartson2017@gmail.com>
2022-01-11 14:51:11 -0500

    completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorList = directors.map((director)=>{
+    return(
+      <div key={director.name}>
+        <h3>Name: {director.name}</h3>
+        Movies:
+        <ul>
+          {director.movies.map((movie)=> {
+            return <li key={movie}>{movie}</li>
+          })}
+        </ul>
+      </div>
+    )
+    }
+  )
+      
+  return(
+    <div>
+      <h1>Directors Page</h1>
+      <div>{directorList}</div>
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,29 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((movie)=> {
+    return (
+      <div key={movie.title}>
+        <h3>Title: {movie.title}</h3>
+        Time: {movie.time} <br></br><br></br>
+        Genres: 
+        <ul>
+          {movie.genres.map((genre)=>{
+            return(
+              <li key={genre}>{genre}</li>
+            );
+          })}
+        </ul>
+
+      </div>
+    )
+  })
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  )
 }
 
 export default Movies;
